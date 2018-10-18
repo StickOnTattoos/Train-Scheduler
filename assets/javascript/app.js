77e455a16b24d7c70e088f801dc3d3920d0c84f3
Tyler <tylerbumguardner@gmail.com>
2018-10-17 18:15:14 -0700

    added beginning functionality

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBKgY5U3f2SFM8fBqROr3B2zS6BoMgfTs4",
+    authDomain: "train-schedule-3e7bf.firebaseapp.com",
+    databaseURL: "https://train-schedule-3e7bf.firebaseio.com",
+    projectId: "train-schedule-3e7bf",
+    storageBucket: "train-schedule-3e7bf.appspot.com",
+    messagingSenderId: "482850456712"
+  };
+  firebase.initializeApp(config);
