9e80dcf8985ec07debb53e8a8e33f8b337e3beed
Tyler Bumguardner <tylerbumguardner@gmail.com>
2018-10-19 20:47:02 -0700

    more functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,45 @@ var config = {
     messagingSenderId: "482850456712"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  $("#newTrain").on("click", function() {
+    event.preventDefault();
+
+    var train = $("#idTrainName").val().trim();
+    var destination = $("#idDestination").val().trim();
+    var firstTrain = $("#idFirstTrain").val().trim();
+    var frequency = $("#idFrequency").val().trim();
+
+    var trainInfo = {
+      fireTrain: train,
+      fireDestination: destination,
+      fireFrequency: frequency,
+      fireFirstTrain: firstTrain,
+      trainTimeInput : database.ServerValue.TIMESTAMP
+    };
+
+    database.ref().push(trainInfo);
+
+    console.log(trainInfo.fireTrain);
+    console.log(trainInfo.fireDestination);
+    console.log(trainInfo.fireFrequency);
+    console.log(trainInfo.fireFirstTrain);
+    console.log(trainInfo.trainTimeInput);
+
+    $("#idTrainName").val("");
+    $("#idDestination").val("");
+    $("#idFirstTrain").val("");
+    $("#idFrequency").val("");
+  })
+
+  database.ref().on("child_added", function (snapshot) {
+    var train = snapshot.val().fireTrain;
+    var destination = snapshot.val().fireDestination;
+    var frequency = snapshot.val().fireFrequency;
+    var firstTrain = snapshot.val().fireFirstTrain;
+
+    var timeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
+    console.log(timeConverted);
+  })
